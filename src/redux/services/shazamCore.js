515bfafd5313fa15c689b46296c542ca3d4d5195
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-06-06 21:13:07 +0530

    initial commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "910b927c0cmsh928749d4a1c5c89p159895jsne2e71d658aed"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: (params) => ({
+        url: "/songs/list-recommendations",
+        params: {
+          key: "484129036",
+          locale: "en-US",
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
